09af42fd5913e91fba1a592c8edc32d70d7e2b32
Mathieu Magalhaes <mathieu.magalhaes@gmail.com>
2023-11-14 18:00:05 +0100

    Lint & Prettier

--- src/lib/countries/andorra.ts
+++ src/lib/countries/andorra.ts
@@ -3,15 +3,15 @@ import { Country } from '../jsvat';
 export const andorra: Country = {
   name: 'Andorra',
   codes: ['AD', 'AND', '020'],
-  calcFn: (vat:string) => {
-    let firstLetter:string = vat.slice(0, 1).toUpperCase();
-    let number:number = parseInt(vat.slice(1, 7));
+  calcFn: (vat: string) => {
+    const firstLetter: string = vat.slice(0, 1).toUpperCase();
+    const number: number = parseInt(vat.slice(1, 7), 10);
 
     if (vat.length !== 8) {
       return false;
     }
     if (!'ACDEFGLOPU'.includes(firstLetter)) {
-      return false
+      return false;
     }
     if (firstLetter === 'F' && number > 699999) {
       return false;
@@ -22,7 +22,7 @@ export const andorra: Country = {
     return true;
   },
   rules: {
-    multipliers:{},
+    multipliers: {},
     regex: [/^(AD)([fealecdgopuFEALECDGOPU]{1}\d{6}[fealecdgoputFEALECDGOPUT]{1})$/]
   }
 };
